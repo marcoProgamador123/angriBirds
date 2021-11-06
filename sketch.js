390acb6c608979444295c2ebbc8a90c2a97857e2
marcoProgamador123 <90981714+marcoProgamador123@users.noreply.github.com>
2021-11-06 18:46:39 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,124 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1, pig2;
+var backgroundImg,platform;
+var slingshot;
+var gameState = "START";
+var bg;
+var score = 0;
+
+
+function preload() {
+    getBackgroundIMG();
+    
+}
+
+function setup(){
+    var canvas = createCanvas(1400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1600,20);
+    platform = new Ground(150, 305, 300, 200);
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    pig1 = new Pig(810,390);
+    pig2 = new Pig(810, 220);
+    box3 = new Box(700,240,70,70);
+    box4 = new Box(920,240,70,70);
+    box5 = new Box(810,160,70,70);
+
+    log1 = new Log(810,260,300, PI/2);
+    log3 =  new Log(810,180,300, PI/2);
+    //log4 = new Log(760,120,150, PI/7);
+    log5 = new Log(800,120,300, PI/2);
+    /*log6 = new Log(1100,170,300,PI/2);
+    log7 = new Log(1100,50,300,PI/2);
+    box6 = new Box(1220,320,70,70);
+    box7 = new Box(1000,320,70,70);
+    box8 = new Box(1000,100,70,70);
+    box9 = new Box(1220,100,70,70);*/
+
+
+    bird = new Bird(50,20);
+    //chain = new Chain(bird.body,log6.body);
+    slingshot = new SlingShot(bird.body,{x:200,y:35});
+
+
+}
+
+function draw(){
+    if(backgroundImg)
+        background(backgroundImg);
+    fill("white");
+    textSize(30);
+    text("score: "+ score,width-300,50);     
+    Engine.update(engine);
+    console.log(box2.body.position.x);
+    console.log(box2.body.position.y);
+    console.log(box2.body.angle);
+    box1.display();
+    box2.display();
+    ground.display();
+    pig1.display();
+    pig1.score();
+    log1.display();
+
+    box3.display();
+    box4.display();
+    pig2.display();
+    pig2.score();
+    //pig3.display();
+    //pig4.display();
+    log3.display();
+
+    box5.display();
+    //log4.display();
+    log5.display();
+    /*log6.display();
+    log7.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();*/
+
+    bird.display();
+    platform.display();
+    slingshot.display();
+}
+function mouseDragged() {
+   if(gameState!=="LANCADO"){
+    Matter.Body.setPosition(bird.body,{x:mouseX,y:mouseY});
+   } 
+    
+}
+function mouseReleased() {
+    slingshot.fly();
+    gameState = "LANCADO";
+}
+function keyPressed(){
+    if(keyCode === 32){
+        //slingshot.attach(bird.body);
+     // gameState="START";  
+    
+    }
+}
+async function getBackgroundIMG(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Tokyo");
+    var responseJson = await response.json()
+    var datetime = responseJson.datetime
+    var hour = datetime.slice(11,13);
+    console.log(hour);
+    if (hour>=06&&hour<=18) {
+      bg = "sprites/bg.png";  
+    } else {
+      bg = "sprites/bg2.jpg";  
+    }
+    backgroundImg = loadImage(bg);
+}
---
+++ teste.js
@@ -0,0 +1,35 @@
+//Exemplos de diferentes tipos de dados JavaScript
+
+//String
+/*var string = "string";
+var string1 = "isso é uma string";
+console.log(string1);
+
+//número
+var numero = 2
+console.log(numero);
+var numero = numero+4;
+console.log(numero);
+
+//boleano
+var bool = true;
+console.log(bool);
+var bool = false;
+console.log(bool);
+
+//indefinido
+var teste;
+console.log(teste);
+
+//nulo
+teste = null;
+console.log(teste);*/
+
+var array = [["Marco", 12, "marco@gmail..."],["Andrea",31,"dejandrea@gmail..."],["Bryan",1,"bryan@g..."],["Marco André",1,"dede@gmail..."]];
+console.log(array);
+
+array.push(["nome",0,"email"]);
+console.log(array);
+
+array.pop();
+console.log(array);
